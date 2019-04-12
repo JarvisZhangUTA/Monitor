46cd7d59063644e105984c1acad1930e4f483b34
jiawei zhang <zhangjw.uta@gmail.com>
2019-04-12 00:30:35 -0500

    serial

--- gateway/src/info.py
+++ gateway/src/info.py
@@ -1,3 +1,14 @@
-import uuid
+def getSerial():
+    cpuserial = "0000000000000000"
+    try:
+        f = open('/proc/cpuinfo','r')
+        for line in f:
+            if line[0:6]=='Serial':
+                cpuserial = line[10:26]
+        f.close()
+    except:
+        cpuserial = "ERROR000000000"
 
-print 'MAC: %s' % uuid.getnode()
+    return cpuserial
+
+print 'Serial: %s' % getSerial()
