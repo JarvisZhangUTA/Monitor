d3b2ec6eedd62a0c81bae56018d7327be7e68da7
jiawei zhang <zhangjw.uta@gmail.com>
2019-04-14 16:53:35 -0500

    test

--- gateway/src/test/receive_worker.py
+++ gateway/src/test/receive_worker.py
@@ -17,14 +17,14 @@ GPIO.output(4, GPIO.HIGH)
 test_response = '02 04 8c 40 34 ff ff ff ff 00 42 01 00 16 05 00 01 01 38 21 73 20 62 53 43 41 36 30 4b 54 4c 2d 44 4f 2f 55 53 2d 34 38 30 00 00 00 5c 10 00 00 00 08 32 00 0d 02 47 01 86 00 0b 04 a6 00 07 00 07 13 5c 13 4d 13 4e 00 13 00 13 00 15 18 56 00 05 03 6b 00 00 03 8a 00 00 02 58 01 b1 01 54 00 00 10 00 20 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 03 ff ff ff ff ff ff ff ff ff ff 08 07 00 00 0c e4 00 28 00 0c 05 2d'
 
 class ReceiveWorker:
-    def __init__(self, config):
+    def __init__(self):
         self.port = serial.Serial(
             "/dev/ttyS0", 
-            self.config.BAUD_RATE,
+            9600,
             parity = serial.PARITY_NONE,
             stopbits = serial.STOPBITS_ONE,
             bytesize = serial.EIGHTBITS,
-            timeout = self.config.SERIAL_WAIT
+            timeout = 2
         )
         self.run()
     
