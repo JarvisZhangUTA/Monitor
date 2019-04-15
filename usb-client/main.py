8baa06248665a5eefb82f65ffee575ee3c95dbbf
jiawei zhang <zhangjw.uta@gmail.com>
2019-04-14 20:48:19 -0500

    test

--- usb-client/main.py
+++ usb-client/main.py
@@ -1,7 +1,9 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QListWidgetItem
 from ui import Ui_MainWindow
-from serial_helper import serial_ports, serial_read, serial_write
+from serial_helper import SerialHelper
+from command_helper import CommandHelper
+from config import config
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
@@ -9,35 +11,81 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         self.initContent()
         self.initEvent()
+        self.serial_helper = None
     
     def initContent(self):
         print 'init content'
         # Badu Rate
         self.BaduRateLineEdit.setText('9600')
         # Com Ports
-        available_ports = serial_ports()
+        available_ports = SerialHelper.serial_ports()
         self.ComPortComboBox.addItems(available_ports)
+        # commands
+        for key, value in config.commands.items():
+            item = QListWidgetItem(key)
+            item.setData(1, value)
+            self.CommandListView.addItem(item)
 
     def initEvent(self):
         print 'init event'
         self.TestConnectionButton.clicked.connect(self.testConnection)
+        self.CommandListView.currentItemChanged.connect(self.commandSelect)
+        self.SendButton.clicked.connect(self.sendCommand)
 
     def testConnection(self):
         print 'test connection'
         badu_rate = self.BaduRateLineEdit.text()
         com_port = self.ComPortComboBox.currentText()
         test_command = '01040000004671F8'
-        
+
         if not badu_rate or not com_port:
             self.Statusbar.showMessage('Badu Rate and Com port needed')
         
-        write_result = serial_write(com_port, badu_rate, test_command)
+        serial_helper = SerialHelper()
+        serial_helper.connect(com_port, badu_rate)
+
+        connected = False
+
+        for addr in range(1, 2):
+            command = '%02d0400000046' % addr
+            crc = CommandHelper.crc16(CommandHelper.toWriteable(command))
+            write_result = serial_helper.serial_write('%s%s' % (command, crc))
+            if write_result:
+                self.Statusbar.showMessage('Find Invertor %02d' % addr)
+                self.serial_helper = serial_helper
+                self.InvertorComboBox.addItem('%02d' % addr)
+                connected = True
+        
+        if connected:
+            self.MessageListView.addItem(QListWidgetItem('Connection Success'))
+        else:
+            self.MessageListView.addItem(QListWidgetItem('Connection Fail'))
+    
+    def commandSelect(self, item):
+        data = item.data(1)
+        self.CommandLineEdit.setText(data)
+    
+    def sendCommand(self):
+        if not self.serial_helper:
+            return self.Statusbar.showMessage('No Connection, test connection first')
+        addr = self.InvertorComboBox.currentText()
+        if not addr:
+            return self.Statusbar.showMessage('No Invertor Selected')
+        command = self.CommandLineEdit.text()
+        if not command:
+            return self.Statusbar.showMessage('Command Required')
+        crc = CommandHelper.crc16(CommandHelper.toWriteable('%s%s' % (addr, command)))
+
+        final_command = '%s%s%s' % (addr, command, crc)
+        write_result = self.serial_helper.serial_write(final_command)
+
+        self.MessageListView.addItem(QListWidgetItem('Write: %s' % final_command))
 
         if write_result:
-            self.MessageListView.addItem(QListWidgetItem('Test Connection receive: ' + write_result))
+            self.MessageListView.addItem(QListWidgetItem('Received: %s' % write_result))
         else:
-            self.MessageListView.addItem(QListWidgetItem('Test Connection no message received'))
-        
+            self.MessageListView.addItem(QListWidgetItem('No Message Received'))
+
 
 # python -m PyQt5.uic.pyuic -x ui.ui -o ui.py
 if __name__ == '__main__':
--- usb-client/serial_helper.py
+++ usb-client/serial_helper.py
@@ -1,60 +1,75 @@
 import sys
 import glob
 import serial
+import time
 
-def serial_ports():
-    """ Lists serial port names
+from command_helper import CommandHelper
 
-        :raises EnvironmentError:
-            On unsupported or unknown platforms
-        :returns:
-            A list of the serial ports available on the system
-    """
-    if sys.platform.startswith('win'):
-        ports = ['COM%s' % (i + 1) for i in range(256)]
-    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
-        # this excludes your current terminal "/dev/tty"
-        ports = glob.glob('/dev/tty[A-Za-z]*')
-    elif sys.platform.startswith('darwin'):
-        ports = glob.glob('/dev/tty.*')
-    else:
-        raise EnvironmentError('Unsupported platform')
+class SerialHelper:
 
-    result = []
-    for port in ports:
-        try:
-            s = serial.Serial(port)
-            s.close()
-            result.append(port)
-        except (OSError, serial.SerialException):
-            pass
-    return result
+    def connect(self, port_name, baud_rate):
+        self.COM_Port = serial.Serial(port=port_name, baudrate=baud_rate, timeout=0)
+        self.COM_Port.bytesize = 8                  # Number of data bits = 8
+        self.COM_Port.parity   = 'N'                # No parity
+        self.COM_Port.stopbits = 1                  # Number of Stop bits = 1
 
-def serial_write(port_name, baud_rate, message):
-    COM_Port = serial.Serial(port=port_name, baudrate=baud_rate, timeout=3)
-    COM_Port.bytesize = 8                  # Number of data bits = 8
-    COM_Port.parity   = 'N'                # No parity
-    COM_Port.stopbits = 1                  # Number of Stop bits = 1
-    COM_Port.setDTR(0) #DTR=0,~DTR=1 so DE = 1,Transmit mode enabled
-    COM_Port.setRTS(0) #RTS=0,~RTS=1 (In FT232 RTS and DTR pins are inverted)
-    data = bytearray(message)
-    NumBytes  = COM_Port.write(data)
-    COM_Port.close()
-    return serial_read(port_name, baud_rate)
+    def serial_write(self, message):
+        print 'Serial write message %s' % message
+        message = CommandHelper.toWriteable(message)
 
-def serial_read(port_name, baud_rate):
-    COM_Port = serial.Serial(port=port_name, baudrate=baud_rate, timeout=3)
-    COM_Port.bytesize = 8                  # Number of data bits = 8
-    COM_Port.parity   = 'N'                # No parity
-    COM_Port.stopbits = 1                  # Number of Stop bits = 1
-    COM_Port.setRTS(1) #RTS=1,~RTS=0 so ~RE=0,Receive mode enabled for MAX485
-    COM_Port.setDTR(1) #DTR=1,~DTR=0 so  DE=0,(In FT232 RTS and DTR pins are inverted)
-                       #~RE and DE LED's on USB2SERIAL board will be off
-    RxedData = COM_Port.readline()
-    COM_Port.close()                       # Close the Serial port
-    return RxedData
+        self.COM_Port.setDTR(0) #DTR=0,~DTR=1 so DE = 1,Transmit mode enabled
+        self.COM_Port.setRTS(0) #RTS=0,~RTS=1 (In FT232 RTS and DTR pins are inverted)
 
+        NumBytes  = self.COM_Port.write(message)
 
-if __name__ == '__main__':
-    print('serial ports: %s' % serial_ports())
+        return self.serial_read()
+    
+    def serial_read(self):
+        self.COM_Port.setRTS(1) #RTS=1,~RTS=0 so ~RE=0,Receive mode enabled for MAX485
+        self.COM_Port.setDTR(1) #DTR=1,~DTR=0 so  DE=0,(In FT232 RTS and DTR pins are inverted)
+                        #~RE and DE LED's on USB2SERIAL board will be off
 
+        WaitTime = 5
+        RxedData = self.COM_Port.read(1)
+        while not RxedData and WaitTime > 0:
+            time.sleep(0.01)
+            WaitTime = WaitTime - 0.01
+            RxedData = self.COM_Port.read(1)
+        
+        AppendData = self.COM_Port.read(1)
+        while AppendData:
+            time.sleep(0.01)
+            RxedData = RxedData + AppendData
+            AppendData = self.COM_Port.read(1)
+        
+        RxedData = CommandHelper.toReadable(RxedData)
+        return RxedData
+
+    @staticmethod
+    def serial_ports():
+        """ Lists serial port names
+
+            :raises EnvironmentError:
+                On unsupported or unknown platforms
+            :returns:
+                A list of the serial ports available on the system
+        """
+        if sys.platform.startswith('win'):
+            ports = ['COM%s' % (i + 1) for i in range(256)]
+        elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+            # this excludes your current terminal "/dev/tty"
+            ports = glob.glob('/dev/tty[A-Za-z]*')
+        elif sys.platform.startswith('darwin'):
+            ports = glob.glob('/dev/tty.*')
+        else:
+            raise EnvironmentError('Unsupported platform')
+
+        result = []
+        for port in ports:
+            try:
+                s = serial.Serial(port)
+                s.close()
+                result.append(port)
+            except (OSError, serial.SerialException):
+                pass
+        return result
